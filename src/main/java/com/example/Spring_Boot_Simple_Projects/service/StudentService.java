61d4bfd008ad8bad9f904f7fc8c97c8ced878291
KRISHNA-JAIS <krishna.jaiswal@knoldus.com>
2022-06-29 15:30:34 +0530

    Second Commit

--- src/main/java/com/example/Spring_Boot_Simple_Projects/service/StudentService.java
+++ src/main/java/com/example/Spring_Boot_Simple_Projects/service/StudentService.java
@@ -14,7 +14,8 @@ public class StudentService {
 
     /**
      *   Changing 1st know about Merging and the Rebase  .
-     *   Changing 1st know about Merging and the Rebase  .
+     *   Changing 2nd know about Merging and the Rebase  .
+     *   Changing 3rd know about Merging and the Rebase  .
      *   StudentRepository
      */
     @Autowired
